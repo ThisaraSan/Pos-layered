431b9a292c1a81935376ba8248d073550f25583e
ThisaraSan <you@example.comthisarasandaruwan@gmail.com>
2023-08-17 12:27:24 +0530

    create Manage Customer panel & load it to the main interface Supermarket by Manage Customer button

---
+++ src/pos/layered/PosLayered.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pos.layered;
+
+import pos.layered.view.CustomerView;
+
+/**
+ *
+ * @author Dell
+ */
+public class PosLayered {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new CustomerView().setVisible(true);
+       
+    }
+    
+}
